53e87e90b426003489d2f5ce492872326f284d62
Jeffrey Duivenvoorden <jheremenis@gmail.com>
2026-01-18 23:20:35 +0100

    chore: removed monaco vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
-	plugins: [monacoEditorPlugin, tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()]
 });
